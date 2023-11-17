46745d02ddb0c91e7abcefe942eee8a774dc8fe0
Abismael <abismaeltapia@gmail.com>
2023-11-16 21:47:51 -0400

    Add some code

--- js/javascript.js
+++ js/javascript.js
@@ -21,7 +21,23 @@ let playerSelection = getPlayerChoice();
 function playRound(computerSelection, playerSelection) {
     if(computerSelection === "rock" && playerSelection === "scissor") {
         return "You Lose! Rock beats Scissor";
+    } else if(computerSelection === "rock" && playerSelection === "paper") {
+        return "You Win! Paper beats Rock";
+    } else if(computerSelection === "scissor" && playerSelection === "rock") {
+        return "You Win! Rock beats Scissor";
+    } else if(computerSelection === "paper" && playerSelection === "rock") {
+        return "You Lose! Paper beats Rock";
+    } else if(computerSelection === "scissor" && playerSelection === "paper") {
+        return "You Lose! Scissor beats Paper";
+    } else if(playerSelection === "rock" && computerSelection === "scissor") {
+        return "You Win! Rock beats Scissor";
+    } else if(playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if(playerSelection === "scissor" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissor";
+    } else if(playerSelection === "scissor" && computerSelection === "paper") {
+        return "You Win! Scissor beats Paper";
     }
 }
 
-console.log(playRound());
+console.log(playRound(playerSelection, computerSelection));
